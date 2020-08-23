660b985a45e4dea99b9dc7460bf16a01be3abe37
Christian Stewart <heeler_cs@hotmail.com>
2020-08-23 20:53:56 +0000

    Fixed the config that vuetify broke

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  publicPath: "",
-  devServer: {
-    host: "0.0.0.0",
-    https: true
-  },
-  transpileDependencies: ["vuetify"]
-};
+	publicPath: '',
+	devServer: {
+		host: '0.0.0.0',
+		https: true,
+		port: process.env.DEV_SERVER_PORT,
+		public: process.env.DEV_SERVER_PUBLIC,
+	},
+	transpileDependencies: ['vuetify'],
+}
