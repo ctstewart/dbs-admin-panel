5112f374213001f67d08793db0f9f411f560d9a2
Christian Stewart <heeler_cs@hotmail.com>
2020-09-10 18:48:35 +0000

    Added app-bar component to the top of every page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-	<router-view />
+	<v-app>
+		<app-bar />
+		<router-view />
+	</v-app>
 </template>
 
 <script>
+import AppBar from './components/AppBar'
 export default {
 	name: 'App',
 
-	components: {},
+	components: { AppBar },
 
 	data: () => ({}),
 }
