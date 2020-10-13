feb54ba5a8f1a11f04386a6213f624fcc0ef208e
Christian Stewart <heeler_cs@hotmail.com>
2020-10-13 17:19:24 -0500

    Removed unused config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,7 @@
 module.exports = {
 	publicPath: '',
 	devServer: {
-		host: '0.0.0.0',
-		https: true,
 		port: process.env.DEV_SERVER_PORT,
-		public: process.env.DEV_SERVER_PUBLIC,
 	},
 	transpileDependencies: ['vuetify'],
 }
