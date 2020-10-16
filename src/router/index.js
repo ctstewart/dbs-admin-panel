b3744be148b897b9fe02768ad507440d4d2137c7
Christian Stewart <heeler_cs@hotmail.com>
2020-10-16 15:40:05 -0500

    Checks auth before routes that require it

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
 import Users from '../views/Users.vue'
 import Devices from '../views/Devices.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -10,21 +12,61 @@ const routes = [
 		path: '/',
 		name: 'Home',
 		component: Users,
+		meta: { requiresAuth: true }
 	},
 	{
 		path: '/users',
 		name: 'Users',
 		component: Users,
+		meta: { requiresAuth: true }
 	},
 	{
 		path: '/devices',
 		name: 'Devices',
 		component: Devices,
+		meta: { requiresAuth: true }
 	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: Login,
+		meta: { requiresAuth: false }
+	}
 ]
 
 const router = new VueRouter({
 	routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+	if (to.meta.requiresAuth) {
+		try {
+			const response = await axios({
+				method: 'get',
+				url: `${process.env.VUE_APP_API_URL}/api/v1/auth/me`,
+				withCredentials: true,
+			})
+
+			if (!response || response.data.success === false) {
+				router.push('/login')
+				next()
+			}
+
+			if (response.data.data.role !== 'admin' && response.data.data.role !== 'superadmin') {
+				router.push('/login')
+				next()
+			}
+
+			next()
+
+		} catch (err) {
+			console.error(err)
+			router.push('/login')
+			next()
+		}
+	} else {
+		next()
+	}
 })
 
 export default router
