d8f9e66a07498d1907f9293c58049d6c2ad59d8d
Christian Stewart <heeler_cs@hotmail.com>
2020-10-28 19:19:01 -0500

    Added title arg

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+	chainWebpack: config => {
+		config
+			.plugin('html')
+			.tap(args => {
+				args[0].title = 'Benefit Sheet | Admin Panel'
+				return args
+			})
+	},
 	publicPath: '',
 	devServer: {
 		port: process.env.DEV_SERVER_PORT,
